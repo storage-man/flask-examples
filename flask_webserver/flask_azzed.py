7fd7be1b4a59cb2b53ceed49a449fb3104049135
storage-man <azzdiiiine@gmail.com>
2021-05-22 23:48:36 +0100

    added status and other things

--- flask_webserver/flask_azzed.py
+++ flask_webserver/flask_azzed.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__,template_folder='.')
 name = "azzedine"
+machines = {
+	    "television": {'name': 'television', 'state': 'GPIO.LOW', 'status': "OFF"},
+    	"machine a laver": {'name': 'machine à laver', 'state': 'GPIO.LOW', 'status': "OFF"},
+    	"lampe1": {'name': 'lampe ', 'state': 'GPIO.LOW',  'status': "OFF"},
+	}
 @app.route("/")
 def hello():
-	return render_template("index.html", message=name);
+	template = {'machines':machines}
+	return render_template("index.html", **template);
+@app.route("/<machine>/<status>")
+def hola(machine,status):
+	print("hi ",machine)
+	if status == "on":
+		machines[machine]['status'] = 'ON'
+		machines[machine]['state'] = 'OFF'
+		print("status ",machines[machine]['status'],machine)
+	else:
+		machines[machine]['status'] = 'OFF'
+		machines[machine]['state'] = 'ON'
+		print("status ",machines[machine]['status'],machine)
+	template = {'machines':machines}
+	return render_template("index.html", **template,hello="after clicking");
 
 if __name__ == "__main__":
     app.run()
