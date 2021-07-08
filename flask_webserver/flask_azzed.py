7e52d92bd81929147aae23e0c77e7b4d2ae2d2c3
Azzedine-prog <aazzedinelakhdar@gmail.com>
2021-07-08 15:21:51 +0100

    from windows

--- flask_webserver/flask_azzed.py
+++ flask_webserver/flask_azzed.py
@@ -1,55 +1,46 @@
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
 app = Flask(__name__,template_folder='.')
-#***********************************************************************
-labels = [
-    'JAN', 'FEB', 'MAR', 'APR',
-    'MAY', 'JUN', 'JUL', 'AUG',
-    'SEP', 'OCT', 'NOV', 'DEC'
-]
-
-values = [
-    967.67, 1190.89, 1079.75, 1349.19,
-    2328.91, 2504.28, 2873.83, 4764.87,
-    4349.29, 6458.30, 9907, 16297
-]
-
-colors = [
-    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
-    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
-    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
-#***********************************************************************
-#render_template("index.html", **template,hello="after clicking")
+GPIO.setwarnings(False)
 name = "azzedine"
+#to use raspberry pi board pin numbers
+GPIO.setmode(GPIO.BCM)
+#set up GPIO output channel
+pins = [23,24,25]
 machines = {
-	    "television": {'name': 'television', 'state': 'GPIO.LOW', 'status': "OFF"},
-    	"machine a laver": {'name': 'machine à laver', 'state': 'GPIO.LOW', 'status': "OFF"},
-    	"lampe1": {'name': 'lampe ', 'state': 'GPIO.LOW',  'status': "OFF"},
+	    "television": {'name': 'television', 'pin':pins[0],'state': GPIO.LOW, 'status': "OFF"},
+    	"machine a laver": {'name': 'machine à laver','pin':pins[1], 'state': GPIO.LOW, 'status': "OFF"},
+    	"lampe1": {'name': 'lampe ','pin':pins[2], 'state': GPIO.LOW,  'status': "OFF"},
 	}
+
+for pin in pins:
+	#print(pin)
+	GPIO.setup(int(pin),GPIO.OUT)
+	GPIO.output(pin,GPIO.LOW)
+
+def set(pin,state):
+	GPIO.output(pin,state)
+#def reset(pin):
+#GPIO.output(pin,GPIO.LOW)
 @app.route("/")
 def hello():
-    line_labels=labels
-    line_values=values
-    template = {'machines':machines,"title":'Bitcoin Monthly Price in USD', "max":17000, "labels":line_labels, "values":line_values};
-    return render_template("index.html", **template);
+	template = {'machines':machines}
+	return render_template("index.html", **template);
 @app.route("/<machine>/<status>")
 def hola(machine,status):
-    print("hi ",machine)
-    if status == "on":
-        machines[machine]['status'] = 'ON'
-        machines[machine]['state'] = 'OFF'
-        print("status ",machines[machine]['status'],machine)
-    else:
-        machines[machine]['status'] = 'OFF'
-        machines[machine]['state'] = 'ON'
-        print("status ",machines[machine]['status'],machine)
-    template = {'machines':machines}
-    #return ;
-    return redirect("/", code=302);
-@app.route("/chart")
-def hello2():
-    line_labels=labels
-    line_values=values
-    template = {'machines':machines}
-    return render_template('index.html',**template)
-def run_my_project():
+	print("hi ",machine)
+	if status == "on":
+		machines[machine]['status'] = 'ON'
+		machines[machine]['state'] = 'OFF'
+		set(machines[machine]['pin'],GPIO.HIGH)
+		print("status ",machines[machine]['status'],machine)
+	else:
+		machines[machine]['status'] = 'OFF'
+		machines[machine]['state'] = 'ON'
+		set(machines[machine]['pin'],GPIO.LOW)
+		print("status ",machines[machine]['status'],machine)
+	template = {'machines':machines}
+	return render_template("index.html", **template,hello="after clicking");
+
+if __name__ == "__main__":
     app.run()
